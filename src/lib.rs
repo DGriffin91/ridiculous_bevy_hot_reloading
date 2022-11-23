fb766b6cf65f554fe161bf22538d0cc2f799412c
DGriffin91 <github@dgdigital.net>
2022-11-22 17:12:34 -0800

    refactor, improve killing cargo watch

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 pub extern crate hot_reloading_macros;
 pub extern crate libloading;
 
-use std::{any::TypeId, time::Duration};
+use std::{any::TypeId, fs::metadata, path::PathBuf, time::Duration};
 
-use bevy::{app::AppExit, prelude::*, utils::Instant, window::WindowCloseRequested};
+use bevy::{prelude::*, utils::Instant};
 use libloading::Library;
 
 /// Get info about HotReload state.
@@ -36,8 +36,8 @@ pub struct HotReloadLibInternalUseOnly {
     pub library: Option<Library>,
     pub updated_this_frame: bool,
     pub last_update_time: Instant,
-    pub cargo_watch_child: Option<std::process::Child>,
-    pub library_name: String,
+    pub cargo_watch_child: Option<ChildGuard>,
+    pub library_paths: LibPathSet,
 }
 
 pub struct HotReloadPlugin {
@@ -50,19 +50,15 @@ pub struct HotReloadPlugin {
     /// name = "lib_your_project_name"
     /// Defaults to your_project_name with lib_ prefix
     /// This should be without .so or .dll
-    pub library_name: String,
+    pub library_name: Option<String>,
 }
 
 impl Default for HotReloadPlugin {
     fn default() -> Self {
-        let lib_path = std::env::current_exe().unwrap();
-        let stem = lib_path.file_stem().unwrap();
-        let lib_stem = format!("lib_{}", stem.to_str().unwrap());
-
         HotReloadPlugin {
             auto_watch: true,
             bevy_dynamic: true,
-            library_name: lib_stem,
+            library_name: None,
         }
     }
 }
@@ -96,7 +92,7 @@ impl Plugin for HotReloadPlugin {
                     ""
                 }
             );
-            child = Some(
+            child = Some(ChildGuard(
                 std::process::Command::new("cargo")
                     .arg("watch")
                     .arg("--postpone")
@@ -107,24 +103,73 @@ impl Plugin for HotReloadPlugin {
                     .arg(build_cmd)
                     .spawn()
                     .expect("cargo watch command failed, make sure cargo watch is installed"),
-            );
+            ));
         }
 
         // TODO move as early as possible
         app.add_system_to_stage(CoreStage::PreUpdate, update_lib)
             .add_system_to_stage(CoreStage::PreUpdate, check_type_ids.after(update_lib))
-            .add_system_to_stage(CoreStage::PostUpdate, clean_up_watch)
+            //.add_system_to_stage(CoreStage::PostUpdate, clean_up_watch)
             .add_event::<HotReloadEvent>()
             .insert_resource(HotReloadLibInternalUseOnly {
                 cargo_watch_child: child,
-                library_name: self.library_name.clone(),
                 library: None,
                 updated_this_frame: false,
                 // Using 1 second ago so to trigger lib load immediately instead of in 1 second
                 last_update_time: Instant::now().checked_sub(Duration::from_secs(1)).unwrap(),
+                library_paths: LibPathSet::new(self.library_name.clone()).unwrap(),
             })
             .insert_resource(HoldTypeId(TypeId::of::<HoldTypeId>()))
             .insert_resource(HotReload::default());
+    }
+}
+
+pub struct LibPathSet {
+    folder: PathBuf,
+    name: String,
+    extension: String,
+}
+
+impl LibPathSet {
+    fn new(library_name: Option<String>) -> Option<Self> {
+        if let Ok(lib_path) = std::env::current_exe() {
+            let name = library_name.unwrap_or({
+                let stem = lib_path.file_stem().unwrap();
+                format!("lib_{}", stem.to_str().unwrap())
+            });
+            let folder = lib_path.parent().unwrap();
+
+            #[cfg(unix)]
+            let extension = String::from("so");
+            #[cfg(windows)]
+            let extension = String::from("dll");
+
+            return Some(LibPathSet {
+                folder: (folder).to_path_buf(),
+                name,
+                extension,
+            });
+        }
+        None
+    }
+
+    /// File path the compiler outputs to
+    fn lib_file_path(&self) -> PathBuf {
+        self.folder.join(&self.name).with_extension(&self.extension)
+    }
+
+    /// File path copied to for hot reloads
+    fn hot_in_use_file_path(&self) -> PathBuf {
+        self.folder
+            .join(format!("{}_hot_in_use", self.name))
+            .with_extension(&self.extension)
+    }
+
+    /// File path copied to for initial run
+    fn main_in_use_file_path(&self) -> PathBuf {
+        self.folder
+            .join(format!("{}_main_in_use", self.name))
+            .with_extension(&self.extension)
     }
 }
 
@@ -138,63 +183,52 @@ fn update_lib(
     if hot_reload.disable_reload {
         return;
     }
-    if let Ok(lib_path) = std::env::current_exe() {
-        let folder = lib_path.parent().unwrap();
-        let lib_stem = &hot_reload_int.library_name;
-        let mut lib_path = folder.join(lib_stem);
-        #[cfg(unix)]
-        lib_path.set_extension("so");
-        #[cfg(windows)]
-        lib_path.set_extension("dll");
-        if lib_path.is_file() {
-            let stem = lib_path.file_stem().unwrap();
-            let lib_stem = format!("{}_hot_in_use", stem.to_str().unwrap());
-            let main_lib_meta = std::fs::metadata(&lib_path).unwrap();
-            let mut hot_lib_path = folder.join(&lib_stem);
-            #[cfg(unix)]
-            hot_lib_path.set_extension("so");
-            #[cfg(windows)]
-            hot_lib_path.set_extension("dll");
-            if hot_lib_path.exists() {
-                let hot_lib_meta = std::fs::metadata(&hot_lib_path).unwrap();
-                if hot_lib_meta.modified().unwrap() < main_lib_meta.modified().unwrap()
-                    && hot_reload_int.last_update_time.elapsed() > Duration::from_secs(1)
-                {
-                    hot_reload_int.library = None;
-                    let _ = std::fs::copy(lib_path, &hot_lib_path);
-                }
-            } else {
-                hot_reload_int.library = None;
-                std::fs::copy(lib_path, &hot_lib_path).unwrap();
-            }
-            if hot_reload_int.library.is_none() {
-                unsafe {
-                    if let Ok(lib) = libloading::Library::new(hot_lib_path) {
-                        // TODO set globals like IoTaskPool here
 
-                        hot_reload_int.library = Some(lib);
-                        hot_reload_int.updated_this_frame = true;
-                        hot_reload_int.last_update_time = Instant::now();
-                        event.send(HotReloadEvent {
-                            last_update_time: hot_reload_int.last_update_time,
-                        });
-                    }
+    let lib_file_path = hot_reload_int.library_paths.lib_file_path();
+    let hot_in_use_file_path = hot_reload_int.library_paths.hot_in_use_file_path();
+
+    // copy over and load lib if it has been updated, or hasn't been initially
+    if lib_file_path.is_file() {
+        if hot_in_use_file_path.is_file() {
+            let hot_lib_meta = metadata(&hot_in_use_file_path).unwrap();
+            let main_lib_meta = metadata(&lib_file_path).unwrap();
+            if hot_lib_meta.modified().unwrap() < main_lib_meta.modified().unwrap()
+                && hot_reload_int.last_update_time.elapsed() > Duration::from_secs(1)
+            {
+                hot_reload_int.library = None;
+                let _ = std::fs::copy(lib_file_path, &hot_in_use_file_path);
+            }
+        } else {
+            hot_reload_int.library = None;
+            std::fs::copy(lib_file_path, &hot_in_use_file_path).unwrap();
+        }
+        if hot_reload_int.library.is_none() {
+            unsafe {
+                if let Ok(lib) = libloading::Library::new(hot_in_use_file_path) {
+                    // TODO set globals like IoTaskPool here
+
+                    hot_reload_int.library = Some(lib);
+                    hot_reload_int.updated_this_frame = true;
+                    hot_reload_int.last_update_time = Instant::now();
+                    event.send(HotReloadEvent {
+                        last_update_time: hot_reload_int.last_update_time,
+                    });
                 }
             }
         }
     }
+
     hot_reload.updated_this_frame = hot_reload_int.updated_this_frame;
     hot_reload.last_update_time = hot_reload_int.last_update_time;
 }
 
-fn clean_up_watch(
-    app_exit: EventReader<AppExit>,
-    window_close: EventReader<WindowCloseRequested>,
-    mut lib_res: ResMut<HotReloadLibInternalUseOnly>,
-) {
-    if !app_exit.is_empty() || !window_close.is_empty() {
-        if let Some(child) = &mut lib_res.cargo_watch_child {
-            child.kill().unwrap();
+pub struct ChildGuard(pub std::process::Child);
+
+impl Drop for ChildGuard {
+    fn drop(&mut self) {
+        match self.0.kill() {
+            Err(e) => println!("Could not kill cargo watch process: {}", e),
+            Ok(_) => println!("Successfully killed cargo watch process"),
         }
     }
 }
@@ -207,14 +241,17 @@ mod ridiculous_bevy_hot_reloading {
 }
 
 #[hot_reloading_macros::make_hot]
-fn check_type_ids(type_id: Res<HoldTypeId>) {
+fn check_type_ids(type_id: Res<HoldTypeId>, _hot_reload_int: Res<HotReloadLibInternalUseOnly>) {
     if type_id.0 != TypeId::of::<HoldTypeId>() {
+        // If we include Res<HotReloadLibInternalUseOnly> the ChildGuard gets dropped
+        // Otherwise cargo watch keeps running
         panic!(
+            "{}",
             "ridiculous_bevy_hot_reloading: ERROR TypeIds \
-            do not match, this happens when the primary \
-            and dynamic libraries are not identically \
-            built. Make sure either both, or neither are \
-            using bevy/dynamic"
+        do not match, this happens when the primary \
+        and dynamic libraries are not identically \
+        built. Make sure either both, or neither are \
+        using bevy/dynamic"
         );
     }
 }
@@ -222,35 +259,21 @@ fn check_type_ids(type_id: Res<HoldTypeId>) {
 /// Copies library file before running so the original can be overwritten
 /// Only needed if using bevy/dynamic
 pub fn dyn_load_main(main_function_name: &str, library_name: Option<String>) {
-    if let Ok(lib_path) = std::env::current_exe() {
-        let lib_stem = library_name.unwrap_or({
-            let stem = lib_path.file_stem().unwrap();
-            format!("lib_{}", stem.to_str().unwrap())
-        });
+    if let Some(lib_paths) = LibPathSet::new(library_name) {
+        let lib_file_path = lib_paths.lib_file_path();
+        let main_in_use_file_path = lib_paths.main_in_use_file_path();
 
-        let folder = lib_path.parent().unwrap();
-        let mut lib_path = folder.join(lib_stem);
-        #[cfg(unix)]
-        lib_path.set_extension("so");
-        #[cfg(windows)]
-        lib_path.set_extension("dll");
-        if lib_path.is_file() {
-            let stem = lib_path.file_stem().unwrap();
-            let lib_stem = format!("{}_main_in_use", stem.to_str().unwrap());
-            let mut hot_lib_path = folder.join(&lib_stem);
-            #[cfg(unix)]
-            hot_lib_path.set_extension("so");
-            #[cfg(windows)]
-            hot_lib_path.set_extension("dll");
-            std::fs::copy(lib_path, &hot_lib_path).unwrap();
-
+        if lib_file_path.is_file() {
+            std::fs::copy(lib_file_path, &main_in_use_file_path).unwrap();
             unsafe {
-                if let Ok(lib) = libloading::Library::new(hot_lib_path) {
+                if let Ok(lib) = libloading::Library::new(main_in_use_file_path) {
                     let func: libloading::Symbol<unsafe extern "C" fn()> =
                         lib.get(main_function_name.as_bytes()).unwrap();
                     func();
                 }
             }
+        } else {
+            panic!("Could not find library file {:?}", lib_file_path);
         }
     }
 }
